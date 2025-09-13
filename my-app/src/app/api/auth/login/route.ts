ee55b194aa5a778d64b5a3b0d1a7a67e3eb45885
kamranjamal <156744268+kamranjamal@users.noreply.github.com>
2025-09-13 02:07:20 +0000

    prodution removed

--- my-app/src/app/api/auth/login/route.ts
+++ my-app/src/app/api/auth/login/route.ts
@@ -44,7 +44,7 @@ export async function POST(request: NextRequest) {
 
     response.cookies.set("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      // secure: process.env.NODE_ENV === "production",
     });
 
     return response;
